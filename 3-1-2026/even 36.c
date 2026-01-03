80e72e6455e5365ac54b40fb139abad02981ef04
kyatindrayatindra-lab <kyatindrayatindra@gmail.com>
2026-01-03 20:09:52 +0530

    Add files via upload

---
+++ 3-1-2026/even 36.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, count = 0;
+    int arr[100];
+
+    // Read number of elements
+    scanf("%d", &n);
+
+    // Read array elements
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Count odd numbers
+    for(i = 0; i < n; i++) {
+        if(arr[i] % 2 != 0) {
+            count++;
+        }
+    }
+
+    // Print result
+    printf("%d", count);
+
+    return 0;
+}
