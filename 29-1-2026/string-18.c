e0e9c5849055aa033cad9ecda43dccf4330f02c7
kyatindrayatindra-lab <kyatindrayatindra@gmail.com>
2026-01-29 21:44:25 +0530

    Add files via upload

---
+++ 29-1-2026/string-18.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int calculateBonus(int years)
+{
+    if (years >= 10)
+        return 15000;
+    else if (years >= 7)
+        return 10000;
+    else if (years >= 5)
+        return 7000;
+    else if (years >= 3)
+        return 3000;
+    else if (years >= 1)
+        return 1000;
+    else
+        return 0;
+}
+
+int main()
+{
+    int years;
+    printf("Enter years of experience: ");
+    scanf("%d", &years);
+
+    printf("Bonus Amount: %d\n", calculateBonus(years));
+    return 0;
+}
