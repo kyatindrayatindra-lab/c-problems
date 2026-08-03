fad582cd12fb1221e5791b32ea3d9120bfd8c4df
kyatindrayatindra-lab <kyatindrayatindra@gmail.com>
2026-08-03 14:02:10 +0530

    Create even and odd number.java

---
+++ even and odd number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int i = sc.nextInt();
+
+        if (i % 2 != 0) {
+            System.out.println("Odd Number");
+        } else {
+            System.out.println("Even Number");
+        }
+
+        sc.close();
+    }
+}
