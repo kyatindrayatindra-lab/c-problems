a30b820543a53998797950b5f34f4e6f400b5f8d
K.Yatindra Nanda kumar <kyatindrayatindra@gmail.com>
2026-08-10 20:43:54 +0530

    Create divisible.java

---
+++ divisible.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> largestDivisibleSubset(int[] nums) {
+        int n = nums.length;
+        if (n == 0) return new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int[] dp = new int[n];
+        int[] prev = new int[n];
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+
+        int maxLen = 1;
+        int maxIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+                maxIndex = i;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        int curr = maxIndex;
+        while (curr != -1) {
+            result.add(nums[curr]);
+            curr = prev[curr];
+        }
+
+        return result;
+    }
+}
