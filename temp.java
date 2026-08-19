a37a49a439860bb4c6559903406b8c212cf13ab2
K.Yatindra Nanda kumar <kyatindrayatindra@gmail.com>
2026-08-19 12:43:20 -0300

    Add Main class to print temperature value

---
+++ temp.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare a short variable and assign a value
+        short temperature = 25;
+
+        // Print the value to the console
+        System.out.println("The stored temperature is: " + temperature + " degrees.");
+    }
+}
