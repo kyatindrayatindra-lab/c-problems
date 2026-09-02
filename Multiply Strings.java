55f02d824e963f61982145998b912fee78e7c183
K.Yatindra Nanda kumar <kyatindrayatindra@gmail.com>
2026-09-02 12:46:34 +0530

    Add multiply method for string representation of numbers
    Implement string multiplication for two non-negative integers.

---
+++ Multiply Strings.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String multiply(String num1, String num2) {
+        if ("0".equals(num1) || "0".equals(num2)) {
+            return "0";
+        }
+
+        int m = num1.length();
+        int n = num2.length();
+        int[] result = new int[m + n];
+
+        for (int i = m - 1; i >= 0; i--) {
+            int d1 = num1.charAt(i) - '0';
+            for (int j = n - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+
+                int mul = d1 * d2;
+                int p1 = i + j;
+                int p2 = i + j + 1;
+
+                int sum = mul + result[p2];
+                result[p2] = sum % 10;
+                result[p1] += sum / 10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int digit : result) {
+            if (!(sb.length() == 0 && digit == 0)) {
+                sb.append(digit);
+            }
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
