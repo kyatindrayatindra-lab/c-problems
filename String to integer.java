97d146c41a3cb77c62514973ad9655ec060aae47
K.Yatindra Nanda kumar <kyatindrayatindra@gmail.com>
2026-09-17 19:51:23 +0530

    Implement string to integer conversion function

---
+++ String to integer.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int myAtoi(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+
+        int i = 0;
+        int n = s.length();
+
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i == n) {
+            return 0;
+        }
+
+        int sign = 1;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = (s.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+
+        int total = 0;
+        while (i < n) {
+            char c = s.charAt(i);
+            if (c < '0' || c > '9') {
+                break;
+            }
+
+            int digit = c - '0';
+
+         
+            if (total > Integer.MAX_VALUE / 10 || 
+               (total == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10)) {
+                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+
+            total = total * 10 + digit;
+            i++;
+        }
+
+        return total * sign;
+    }
+}
